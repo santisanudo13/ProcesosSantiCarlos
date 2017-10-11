958c00e5cad2a5910a4ab254ad72cf9c5ed225ee
Santiago Sañudo Martinez <santisanudo@gmail.com>
2017-10-11 11:39:04 +0200

    Pruebas bien hechas

--- Practica1/ImpuestoCirculacion/src/test/java/AceptacionYSistemaTest.java
+++ Practica1/ImpuestoCirculacion/src/test/java/AceptacionYSistemaTest.java
@@ -1,4 +1,6 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 
@@ -9,44 +11,59 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import es.unican.ps.impuestoCirculacion.businessLayer.GestionImpuestoCirculacion;
-import es.unican.ps.impuestoCirculacion.daoLayer.ImpuestosDAO;
+import es.unican.ps.impuestoCirculacion.daoLayer.IContribuyentesDAO;
+import es.unican.ps.impuestoCirculacion.daoLayer.IVehiculosDAO;
 import es.unican.ps.impuestoCirculacion.domain.Contribuyente;
 import es.unican.ps.impuestoCirculacion.domain.Vehiculo;
 
 public class AceptacionYSistemaTest {
-	
+
 	private static GestionImpuestoCirculacion gestion;
+	private static IContribuyentesDAO contribuyentes =  mock(IContribuyentesDAO.class);
+	private static IVehiculosDAO vehiculos =  mock(IVehiculosDAO.class);
+	private static Contribuyente as1a, as1b;
+
 	@Before
 	public void beforeMethods(){
-		
+
 	}
-	
+
 	@After
 	public void afterMethods(){
-		
+
 	}
-	
+
 	@BeforeClass
 	public static void init(){
-		gestion = new GestionImpuestoCirculacion(new ImpuestosDAO(), new ImpuestosDAO());
+		//		Gestion
+		gestion = new GestionImpuestoCirculacion(contribuyentes, vehiculos );
+
+		//		Contribuyentes
+		as1a = new Contribuyente(new ArrayList<Vehiculo>(), "Lidia", "Lopez", "Revuelta", "72081145Z");
+		as1b = new Contribuyente(new ArrayList<Vehiculo>(), "Victor", "Gomez", "Cobo", "72189333S");
+
+		//		Que hacen los mocks cuando se llama a sus metodos
+		when(contribuyentes.creaContribuyente(as1a)).thenReturn(as1a); // Caso as1a
+		when(contribuyentes.creaContribuyente(as1b)).thenReturn(null); //Caso as1b
+
 	}
 	@AfterClass
 	public static void end(){
-		gestion.bajaContribuyente("72081145Z");
 	}
-	
-	
+
+
 	/**
-	 * Contribuyente â€œLidia Lopez Revuelta 72081145Zâ€� intenta darse de alta -> se da de alta correctamente
+	 * Contribuyente Lidia Lopez Revuelta 72081145Zâ€� intenta darse de alta -> se da de alta correctamente
 	 */
 	@Test
 	public void as1a(){
-		
-		Contribuyente cInput = new Contribuyente(new ArrayList<Vehiculo>(), "Lidia", "Lopez", "Revuelta", "72081145Z");
-		Contribuyente cOutput = gestion.altaContribuyente(cInput);
-		
+
+
+
+		Contribuyente cOutput = gestion.altaContribuyente(as1a);
+
 		assertTrue(cOutput != null);
-		
+
 	}
 	/**
 	 * AS1.b Contribuyente “Víctor Gómez Cobo 72189333S” intenta darse de alta -> se da un error
@@ -54,11 +71,11 @@ porque ya existe
 	 */
 	@Test
 	public void as1b(){
-		
+
 		Contribuyente cInput = new Contribuyente(new ArrayList<Vehiculo>(), "Victor", "Gomez", "Cobo", "72189333S");
 		Contribuyente cOutput = gestion.altaContribuyente(cInput);
-		
+
 		assertTrue(cOutput == null);
-		
+
 	}
 }
